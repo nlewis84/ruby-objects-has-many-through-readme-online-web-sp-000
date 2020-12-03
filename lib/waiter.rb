e6ae100bdb2958187d03ec30486403859653832d
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-03 04:55:27 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -32,4 +32,29 @@ class Waiter
     end
     best_tipped_meal.customer
   end
+  
+  def frequent_customer
+    customer_list = self.meals.collect {|meal| meal.customer}
+    customer_list.max_by {|i| customer_list.count(i)}
+  end
+  
+  def worst_tipped_meal
+      worst_tipped_meal = meals.min do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    worst_tipped_meal
+  end
+  
+  def self.most_experienced_avg_tips
+    most_experienced = self.all.max_by {|waiter| waiter.yrs_experience}
+    meal_tips = self.meals.collect {|meal| meal.tip}
+    meal_tips.inject {|sum, el| sum + el} / meal_tips.size
+    
+  end
+  
+  def self.least_experienced_avg_tips
+    least_experienced = self.all.min_by {|waiter| waiter.yrs_experience}
+    meal_tips = self.meals.collect {|meal| meal.tip}
+    meal_tips.inject {|sum, el| sum + el} / meal_tips.size
+  end
 end
